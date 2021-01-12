c810b513e8f3702dd7dd466612387a6a3f9c025e
Michael Lock <unity.mobile.development@gmail.com>
2021-01-12 13:18:33 +0200

    added onitemclicklistener

--- app/src/main/java/com/example/randomnumadapter_recyclerview_fragment/RandomNumListAdapter.kt
+++ app/src/main/java/com/example/randomnumadapter_recyclerview_fragment/RandomNumListAdapter.kt
@@ -2,15 +2,18 @@ package com.example.randomnumadapter_recyclerview_fragment
 
 import android.provider.Settings
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
+import kotlin.collections.ArrayList
 
 
-class RandomNumListAdapter(seed: Int) : RecyclerView.Adapter<RecyclerViewHolder>() {
-    private val random: Random = Random(seed.toLong())
-
-
+class RandomNumListAdapter(
+    private val presidents: MutableList<President>,
+    private val listener: OnItemClickListener
+) : RecyclerView.Adapter<RandomNumListAdapter.RecyclerViewHolder>() {
 
     override fun getItemViewType(position: Int): Int {
         return R.layout.frame_textview
@@ -24,17 +27,39 @@ class RandomNumListAdapter(seed: Int) : RecyclerView.Adapter<RecyclerViewHolder>
 
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
 
-        val president: President = GlobalModel.presidents[position]
+        val president: President = presidents[position]
 
         holder.view.text = president.name
+
 
 //        holder.view.text = random.nextInt().toString()
 
 
     }
 
-    override fun getItemCount(): Int {
+     override fun getItemCount(): Int {
         return GlobalModel.presidents.size
     }
 
+    inner class RecyclerViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        val view: TextView = itemView.findViewById(R.id.randomText)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position: Int = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
 }
+
--- app/src/main/java/com/example/randomnumadapter_recyclerview_fragment/SecondFragment.kt
+++ app/src/main/java/com/example/randomnumadapter_recyclerview_fragment/SecondFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.function.Predicate
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SecondFragment : Fragment(), RandomNumListAdapter.OnItemClickListener {
 
 
     private lateinit var recyclerView: RecyclerView
+
+    val presidents: MutableList<President> = GlobalModel.presidents
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,10 +37,17 @@ class SecondFragment : Fragment() {
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(view.context)
 
-        recyclerView.adapter = RandomNumListAdapter(1234)
+        recyclerView.adapter = RandomNumListAdapter(presidents, this)
 
         return view
-/*        return inflater.inflate(R.layout.fragment_second, container, false)*/
+    }
+
+    override fun onItemClick(position: Int) {
+        val clickedItem: President = presidents[position]
+
+        clickedItem.name = "Clicked"
+
+        recyclerView.adapter?.notifyItemChanged(position)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
